95c3822f998434c8410843ea30ac842db89d8b8e
alasyariyphysics <alasyariyphysics@gmail.com>
2025-12-11 09:03:50 +0700

    Update whiteboard.js

--- assets/js/simulations/whiteboard.js
+++ assets/js/simulations/whiteboard.js
@@ -1,32 +1,33 @@
 // File: assets/js/simulations/whiteboard.js
 
+// Membuat fungsi instance p5.js untuk Whiteboard
 let whiteboard = (p) => {
+  let canvasWidth = 600;
+  let canvasHeight = 400;
+
   p.setup = () => {
-    let canvas = p.createCanvas(600, 400); // Ukuran lebih besar untuk papan tulis
-    canvas.parent('whiteboard-holder');
-    p.background(255); // Putih bersih
+    // Memasukkan kanvas ke div id="whiteboard-canvas-holder"
+    let canvas = p.createCanvas(canvasWidth, canvasHeight);
+    canvas.parent('whiteboard-canvas-holder');
+    p.background(255); // Latar belakang putih bersih
   };
 
   p.draw = () => {
-    // Fungsi draw whiteboard ini kosong, kita hanya butuh mouse input
+    // Fungsi draw kosong, hanya digunakan untuk menerima input mouse
   };
 
   p.mouseDragged = () => {
-    // Hanya menggambar jika tombol mouse ditekan dan diseret
+    // Menggambar garis yang mengikuti gerakan mouse
     p.stroke(0); // Warna hitam
-    p.strokeWeight(3); // Ketebalan garis
-    p.line(p.pmouseX, p.pmouseY, p.mouseX, p.mouseY); // Menggambar garis dari posisi mouse sebelumnya ke posisi mouse saat ini
-  };
-
-  p.mousePressed = () => {
-    // Ini mencegah penyeretan mouse memicu aksi default browser
-    return false;
+    p.strokeWeight(3); // Ketebalan
+    p.line(p.pmouseX, p.pmouseY, p.mouseX, p.mouseY);
   };
   
-  // Fungsi untuk menghapus papan tulis
+  // Fungsi ini dipanggil dari tombol 'Clear Whiteboard' di HTML
   window.clearWhiteboard = () => {
-    p.background(255);
+    p.background(255); // Mengisi ulang kanvas dengan warna putih
   };
 };
 
-let myWhiteboard = new p5(whiteboard); // Membuat instance p5 untuk Whiteboard
+// Menginisialisasi instance Whiteboard
+let myWhiteboard = new p5(whiteboard);
